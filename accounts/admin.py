8f126b3821c69b81bc3499c006ba4bf62049dcb0
Joao Batista <joaobldss@gmail.com>
2025-07-20 14:33:45 -0300

    feat: registering the customer and seller models in the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,4 +5,14 @@ from models import Customer, Saller
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'cpf', 'telephone', 'address',]
-    
+    search_fields = ['id', 'user', 'cpf',]
+    list_filter = ['user', 'cpf', 'telephone', 'address',]
+    ordering = '-id',
+
+
+@admin.register(Saller)
+class SallerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'customer', 'telephone',]
+    search_fields = ['id', 'name', 'customer',]
+    list_filter = ['name', 'customer',]
+    ordering = '-id'
