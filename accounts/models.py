18ddf1fd1c895e59a0cf76679cd6dd1db3bdb932
Joao Batista <joaobldss@gmail.com>
2025-07-20 10:36:20 -0300

    feat: creating customer models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    cpf = models.IntegerField(max_length=11, unique=True)
+    telephone = models.IntegerField(max_length=9)
+    address = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+        return self.user
+    
+
+class Saller(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null= True)
+    telephone = models.IntegerField(max_length=15)
+
+    class Meta:
+        verbose_name = 'Vendedor'
+        verbose_name_plural = 'Vendedores'
+
+    def __str__(self):
+        return self.name
