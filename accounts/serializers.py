75d3c98ad3673a52431aff9346a0377279d28280
Joao Batista <joaobldss@gmail.com>
2025-07-25 19:58:57 -0300

    creating the account serializers file and creating your serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from accounts.models import Customer, Saller
+
+
+class CustomerSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email', 'cpf', 'telephone', 'address', 'created_at', 'update_at',]
+
+
+class SallerSerializers(serializers.ModelSerializer):
+        class Meta:
+             model = Saller
+             fields = ['name', 'customer', 'telephone', 'created_at', 'update_at',]
