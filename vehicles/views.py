4d68b5d78826da5831277f9f6bc01e57e345e78c
Joao Batista <joaobldss@gmail.com>
2025-07-25 20:24:08 -0300

    feat: creating vehicle viewsets

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from vehicles.models import Vehicle, TestDrive
+from vehicles.serializers import VehicleSerializers, TestDriveSerializers
 
-# Create your views here.
+
+class VehicleApiv1ViewSet(ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializers
+
+
+class TestDriveApiv1ViewSet(ModelViewSet):
+    queryset = TestDrive.objects.all()
+    serializer_class = TestDriveSerializers
