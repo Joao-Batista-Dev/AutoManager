b5cdb910400b1b326f4db6c9823abd5e17207871
Joao Batista <joaobldss@gmail.com>
2025-07-25 19:48:45 -0300

    feat: installing the django rest framework

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -7,6 +7,7 @@ class VehicleAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'brand', 'year', 'color', 'mileage', 'price', 'description', 'available', 'created_at', 'update_at',]
     search_fields =  ['id', 'name', 'brand', 'year',  'created_at',]
     list_filter =  ['id', 'name', 'brand', 'year',  'created_at',]
+    list_editable = ['available']
     ordering = ['-id',]
 
 
