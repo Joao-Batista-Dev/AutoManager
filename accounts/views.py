92406873c3251c6f1022b8e07a841fade67cc9d9
Joao Batista <joaobldss@gmail.com>
2025-07-25 20:49:20 -0300

    feat: creating the account viewset

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from accounts.models import Customer, Saller
+from accounts.serializers import CustomerSerializers, SallerSerializers
 
-# Create your views here.
+
+class CustomerApiV1ViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializers
+
+
+class SallerApiV1ViewSet(ModelViewSet):
+    queryset = Saller.objects.all()
+    serializer_class = SallerSerializers
