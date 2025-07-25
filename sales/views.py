b707b18b276373d1e92af1be947b9e936b3185fe
Joao Batista <joaobldss@gmail.com>
2025-07-25 20:29:52 -0300

    feat: creating the sales viewset

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from sales.models import Vend, SalesProposal
+from sales.serializers import VendSerializers, SalesProposalSerializers
 
-# Create your views here.
+
+class VendApiV1ViewSet(ModelViewSet):
+    queryset = Vend.objects.all()
+    serializer_class = VendSerializers
+
+
+class SalesProposalApiV1ViewSet(ModelViewSet):
+    queryset = SalesProposal
+    serializer_class = SalesProposalSerializers
