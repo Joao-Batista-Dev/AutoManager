b0804a2ff10e3149b8710ce4e33cbe1ed34178d6
Joao Batista <joaobldss@gmail.com>
2025-07-26 10:03:06 -0300

    feat: Creating CPF and Telephone validation for customer and seller accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,8 +7,23 @@ class CustomerSerializers(serializers.ModelSerializer):
         model = Customer
         fields = ['id', 'name', 'email', 'cpf', 'telephone', 'address', 'created_at', 'update_at',]
 
+        def validate_cpf(self, value):
+            if len(value) != 11:
+                raise serializers.ValidationError('CPF deve ter 11 dígitos.')
+            return value
+        
+        def validate_telephone(self, value):
+            if len(value) != 9:
+                raise serializers.ValidationError('Seu Telefone dever conter 9 dígitos')
+            return value
+
 
 class SallerSerializers(serializers.ModelSerializer):
         class Meta:
-             model = Saller
-             fields = ['id', 'name', 'customer', 'telephone', 'created_at', 'update_at',]
+            model = Saller
+            fields = ['id', 'name', 'customer', 'telephone', 'created_at', 'update_at',]
+
+        def validate_telephone(self, value):
+            if len(value) != 9:
+                raise serializers.ValidationError('Seu Telefone dever conter 9 dígitos')
+            return value
